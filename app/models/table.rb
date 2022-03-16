dd073893651229ff68e381884240dc9250bdd6d2
Cameron <reid.camerond@gmail.com>
2022-03-16 12:18:03 +1100

    seed file

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,6 +1,6 @@
 class Table < ApplicationRecord
-  has_many :foods, optional: true 
-  has_many :drinks, optional: true
-  belongs_to :user 
+  has_many :foods
+  has_many :drinks
+  belongs_to :server, optional: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,99 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Drink.destroy_all
+print 'creating drinks .....'
+
+d1 = Drink.create!(
+  name: "Coffee",
+  price: 4,
+  quantity: 10
+)
+
+d2 = Drink.create!(
+  name: "Tea",
+  price: 3.5,
+  quantity: 15
+)
+
+d3 = Drink.create!(
+  name: "Orange Juice",
+  price: 7,
+  quantity: 6
+)
+
+puts "created #{Drink.count } drinks."
+
+Food.destroy_all
+print 'creating food items .....'
+
+f1 = Food.create!(
+  name: "burger",
+  price: 14,
+  quantity: 10
+)
+
+f2 = Food.create!(
+  name: "Pasta",
+  price: 35,
+  quantity: 15
+)
+
+f3 = Food.create!(
+  name: "Garlic Bread",
+  price: 7,
+  quantity: 6
+)
+
+puts "created #{Food.count } food items."
+
+Server.destroy_all
+print 'creating Servers .....'
+
+s1 = Server.create!(
+  name: "Tom",
+  pin: 1234,
+  isAdmin: true
+)
+
+s2 = Server.create!(
+  name: "Bob",
+  pin: 0000,
+  isAdmin: false
+)
+
+s3 = Server.create!(
+  name: "Cam",
+  pin: 9999,
+  isAdmin: false
+)
+
+puts "created #{Server.count } servers."
+
+Table.destroy_all
+print 'creating tables .....'
+
+t1 = Table.create!(
+  number: 1
+)
+
+t2 = Table.create!(
+  number: 2
+)
+
+t3 = Table.create!(
+  number: 3
+)
+
+t4 = Table.create!(
+  number: 4
+)
+
+t5 = Table.create!(
+  number: 5
+)
+
+t6 = Table.create!(
+  number: 6
+)
+
+puts "created #{Table.count } tables."
+
+
